9d1dd6a66eb68c6ef45ce7f4a228eadb681915a0
jefferson <jschafuelan@espe.edu.ec>
2026-06-16 20:28:21 -0500

    cambio en archivo server

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${PORT}/graphql`);
+  console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
